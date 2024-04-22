608d06e015ea8f559f5c6977c9e734394078b67e
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-04-21 22:24:01 -0300

    commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import "./Button.css";
+
+const Button = ({id, text, action}) => {
+  const handleAction = (e) => {
+    action(e);
+  };
+
+  return (
+    <button id={id} onClick={handleAction}>
+      {text}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  id: PropTypes.string.isRequired, // Validando que a prop id é uma string e obrigatória
+  text: PropTypes.string.isRequired, // Validando que a prop text é uma string e obrigatória
+  action: PropTypes.func.isRequired, // Validando que a prop action é uma função e obrigatória
+};
+
+export default Button;
